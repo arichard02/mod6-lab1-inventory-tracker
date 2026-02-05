f0b4f076e09106c9fb05e3d8de0ec2fcc9824cd3
Adwaina <arichard02@aol.com>
2026-02-05 07:09:07 -0500

    deleted old unused  code

--- main.ts
+++ main.ts
@@ -1,22 +1,3 @@
-// import { DigitalProduct } from "./models/DigitalProduct";
-// import { PhysicalProduct } from "./models/PhysicalProduct";
-// import { calculateTax } from "./utils/taxCalculator";
-// import {Product} from "./models/Product";
-
-// const physicalProduct = new PhysicalProduct("1234", "gum", 10, 20);
-// const digitalProduct = new DigitalProduct("5678", "e-book", 10, 30);
-
-// const arr = [physicalProduct, digitalProduct];
-
-// arr.forEach((element) => {
-//   console.log("Details", element.displayDetails());
-//   let finalPrice = calculateTax(element);
-//   console.log("calculate prices with tax", finalPrice);
-//   console.log(
-//     `The price is ${element.price} and the final price after tax for ${element.sku}${element.name} will be ${finalPrice}`,
-//   );
-// });
-
 import { DigitalProduct } from "./models/DigitalProduct.js";
 import { PhysicalProduct } from "./models/PhysicalProduct.js";
 import {Product} from "./models/Product.js";
@@ -33,7 +14,3 @@ products.forEach((product) => {
   console.log(".......");
 });
 
-// console.log(
-//   `Final price for ${element.displayDetails()} is ${finalPrice}`
-// );
-// ${element.sku} (${element.name})
